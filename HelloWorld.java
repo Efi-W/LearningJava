21713bfcb933be4c59734d1df065d6e5064a9f53
EfiW <efi.whiteley@gmail.com>
2024-09-12 15:55:57 +0100

    Commit 3 - Data Types

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,7 +3,21 @@ public class HelloWorld {
     public static void main(String args[])
     {
         int num = 3;
-        int num2 = 7;
-        System.out.println(num * num2);
+        byte byte1 = 98;
+        short short1 = 32;
+        long long1 = 5853L;
+        float float1 = 2.5f;
+        double double1 = 3.4546;
+        char c1 = 'E';
+        boolean boolean1 = false;
+
+        System.out.println(num);
+        System.out.println(byte1);
+        System.out.println(short1);
+        System.out.println(long1);
+        System.out.println(float1);
+        System.out.println(double1);
+        System.out.println(c1);
+        System.out.println(boolean1);
     }
 }
