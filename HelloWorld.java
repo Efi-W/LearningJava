788e8523eed2ead7d66125e4e9ecb9d166974476
EfiW <efi.whiteley@gmail.com>
2024-09-12 15:33:56 +0100

    Commit 2 - Variables

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,9 @@
 public class HelloWorld {
 
-    public static void main(String args[]){
-        System.out.println("Hello World!");
+    public static void main(String args[])
+    {
+        int num = 3;
+        int num2 = 7;
+        System.out.println(num * num2);
     }
 }
