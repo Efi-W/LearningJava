58853c5b3538137c6febc3bffa9795e002ee1fa1
efiwh <efi.whiteley@gmail.com>
2024-09-14 14:05:06 +0100

    Commit 5 - Operators

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,26 +2,19 @@ public class HelloWorld {
 
     public static void main(String args[])
     {
-        //int int1 = 9;
-        //double double1 = int1;
+        int a = 5+10;
+        int b = 10-5;
+        int c = 10/3;
+        int d = 10*5;
 
-        //double double2 = 5.63d;
-        //int int2 = (int) double2;
+        System.out.println(a < b);
+        System.out.println(d >= a);
+        System.out.println(c == c);
+        System.out.println(a < 10 && b > 2);
 
-        int maxPoints = 1000;
-        int userPoints = 730;
-
-        float percentage = (float) maxPoints / userPoints * 100;
-
-        System.out.println("The Percentage is.. " + percentage + "%");
-
-
-
-        //System.out.println(int1);
-        //System.out.println(double1);
-
-        //System.out.println(double2);
-        //System.out.println(int2);
+        System.out.println(++a);
+        System.out.println(--a);
+        System.out.println(d%c);
 
 
     }
