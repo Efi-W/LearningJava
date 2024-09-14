78898ddf0e2f9b85b3005e92ab59ed2adf514a04
efiwh <efi.whiteley@gmail.com>
2024-09-14 14:29:34 +0100

    Commit 6 - Strings

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,19 +2,76 @@ public class HelloWorld {
 
     public static void main(String args[])
     {
-        int a = 5+10;
-        int b = 10-5;
-        int c = 10/3;
-        int d = 10*5;
+       //String greeting = "Hello";
+       //String sentence = "Hello, how are you";
 
-        System.out.println(a < b);
-        System.out.println(d >= a);
-        System.out.println(c == c);
-        System.out.println(a < 10 && b > 2);
+       //System.out.println(greeting.length());
+       //System.out.println(greeting.toUpperCase());
+       //System.out.println(greeting.toLowerCase());
 
-        System.out.println(++a);
-        System.out.println(--a);
-        System.out.println(d%c);
+       //System.out.println(sentence.indexOf("are"));
+
+        //-----------------String Methods---------------------------
+
+        // String firstName = "John";
+        //String lastName = "Smith";
+
+        //System.out.println(firstName + " " + lastName);
+        //System.out.println(firstName.concat(lastName));
+
+        //String a = "10";
+        //String b = "7";
+        //String c = a + b;
+       //int d= 20;
+        //String e = d + b;
+
+        //System.out.println(c);
+       //System.out.println(e);
+
+        //----------------------Concatenation-----------------------------
+
+        //String txt = "Its saturday today";
+        //String txt2 = "Its \'saturday\' today";
+        //String txt3 = "Its \"saturday\" today";
+        //String txt4 = "It\'s saturday today";
+        //String txt5 = "Its saturday\\ today";
+
+        //System.out.println(txt);
+        //System.out.println(txt2);
+        //System.out.println(txt3);
+        //System.out.println(txt4);
+        //System.out.println(txt5);
+
+        //-----------------Special Characters-----------------
+
+        String txt = "\nIt's Saturday Today";
+        String txt2 = "\rIt's Saturday Today";
+        String txt3 = "\tIt's Saturday Today";
+        String txt4 = "\nIt's Saturday Today";
+        String txt5 = "\bIt's Saturday Today";
+        String txt6 = "\fIt's Saturday Today";
+
+        System.out.println(txt);
+        System.out.println(txt2);
+        System.out.println(txt3);
+        System.out.println(txt4);
+        System.out.println(txt5);
+        System.out.println(txt6);
+
+
+        //---------------------Extra---------------------
+
+
+
+
+
+
+
+
+
+
+
+
 
 
     }
