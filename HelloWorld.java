660e60ad796254b8ee8d34547902ee5de7d0f13a
EfiW <efi.whiteley@gmail.com>
2024-09-18 13:50:05 +0100

    Commit 11 - While Loops

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,41 +2,23 @@ public class HelloWorld {
 
     public static void main(String args[])
     {
-      int day = 9;
+      int i = 1;
 
-      switch(day)
+      while (i <= 5)
       {
-          case 1:
-              System.out.println("Monday");
-              break;
+         System.out.println("Outer loop " + i);
+         int j = 1;
+         while (j<=2)
+         {
+             System.out.println("Inner loop " + j);
+             j++; //increment
+         }
 
-          case 2:
-              System.out.println("Tuesday");
-              break;
 
-          case 3:
-              System.out.println("Wednesday");
-              break;
-
-          case 4:
-              System.out.println("Thursday");
-              break;
-
-          case 5:
-              System.out.println("Friday");
-              break;
-
-          case 6:
-              System.out.println("Saturday");
-              break;
-
-          case 7:
-              System.out.println("Sunday");
-              break;
-
-          default:
-              System.out.println("No day for that number");
+         i++; //increment
       }
+
+
 
 
 
