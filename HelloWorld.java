12da8947a4be288d8d502b20c8a8240008ce2c1e
EfiW <efi.whiteley@gmail.com>
2024-09-18 09:41:46 +0100

    Commit 7 - Math functions

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,75 +2,15 @@ public class HelloWorld {
 
     public static void main(String args[])
     {
-       //String greeting = "Hello";
-       //String sentence = "Hello, how are you";
 
-       //System.out.println(greeting.length());
-       //System.out.println(greeting.toUpperCase());
-       //System.out.println(greeting.toLowerCase());
+        Math.max(12,20);  // Finds max number
+        Math.min(12,20);  //Finds min number
+        Math.sqrt(144);  //Square root
+        Math.abs(-2.5);  // Turns negative to positive
+        Math.random();  // Returns random number
 
-       //System.out.println(sentence.indexOf("are"));
-
-        //-----------------String Methods---------------------------
-
-        // String firstName = "John";
-        //String lastName = "Smith";
-
-        //System.out.println(firstName + " " + lastName);
-        //System.out.println(firstName.concat(lastName));
-
-        //String a = "10";
-        //String b = "7";
-        //String c = a + b;
-       //int d= 20;
-        //String e = d + b;
-
-        //System.out.println(c);
-       //System.out.println(e);
-
-        //----------------------Concatenation-----------------------------
-
-        //String txt = "Its saturday today";
-        //String txt2 = "Its \'saturday\' today";
-        //String txt3 = "Its \"saturday\" today";
-        //String txt4 = "It\'s saturday today";
-        //String txt5 = "Its saturday\\ today";
-
-        //System.out.println(txt);
-        //System.out.println(txt2);
-        //System.out.println(txt3);
-        //System.out.println(txt4);
-        //System.out.println(txt5);
-
-        //-----------------Special Characters-----------------
-
-        String txt = "\nIt's Saturday Today";
-        String txt2 = "\rIt's Saturday Today";
-        String txt3 = "\tIt's Saturday Today";
-        String txt4 = "\nIt's Saturday Today";
-        String txt5 = "\bIt's Saturday Today";
-        String txt6 = "\fIt's Saturday Today";
-
-        System.out.println(txt);
-        System.out.println(txt2);
-        System.out.println(txt3);
-        System.out.println(txt4);
-        System.out.println(txt5);
-        System.out.println(txt6);
-
-
-        //---------------------Extra---------------------
-
-
-
-
-
-
-
-
-
-
-
+        int randomNum = (int)(Math.random() * 101); // Prints Random number 0-100
+        System.out.println(randomNum);
 
 
 
