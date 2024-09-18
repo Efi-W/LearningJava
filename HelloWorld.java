272524e5a40734f82c642e361e74dd1f03131b21
EfiW <efi.whiteley@gmail.com>
2024-09-18 10:13:01 +0100

    Commit 9 - If/Elseif/Else

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,24 +2,20 @@ public class HelloWorld {
 
     public static void main(String args[])
     {
-        //boolean isThisTrue = true;
-        //boolean isThisAlsoTrue = false;
+        int time = 6;
 
-       // System.out.println(isThisTrue);
-        //System.out.println(isThisAlsoTrue);
-
-        int a = 10;
-        int b = 17;
-        int x = 7;
-        int y = 6;
-
-        boolean result = a>b && x<y; //and
-        boolean result2 = a>y || b==y; //or
-
-        System.out.println(result);
-        System.out.println(result2);
-
-
+        if (time < 10)
+        {
+            System.out.println("Good Morning");
+        }
+        else if ( time < 18)
+        {
+            System.out.println("Good Day");
+        }
+        else
+        {
+            System.out.println("Good Evening");
+        }
 
 
     }
