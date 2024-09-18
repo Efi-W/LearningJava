a05f0432e9d713235ddf96f09e6552b2cc1b9280
EfiW <efi.whiteley@gmail.com>
2024-09-18 14:40:31 +0100

    Commit 13 - For Loops

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,21 +2,16 @@ public class HelloWorld {
 
     public static void main(String args[])
     {
+        for (int i = 1; i <= 5; i++){
 
-        int dice = 1;
-        while (dice <= 5){
+            System.out.println("Day " + i);
 
-            if (dice < 5){
+            for (int j = 1; j<=3; j++){
 
-                System.out.println("Dice less than 5");
+                System.out.println(j + " Hours");
             }
-            else{
-
-                System.out.println("Dice greater or equal to 5");
-            }
-
-            dice = dice+1;
         }
+
 
     }
 }
