478d66243c0377f7c8c90401f54d961e8a101c30
EfiW <efi.whiteley@gmail.com>
2024-09-25 14:06:00 +0100

    Arrays

---
+++ Example.java
@@ -0,0 +1,15 @@
+public class Example
+{
+    public static void main(String[] args)
+    {
+
+       String[] myStrings = {"A","B","C","D"};
+       int[] myInts = {3,4,5,6};
+       myStrings[0] = "Hi";
+
+        System.out.println(myStrings[0]);
+        System.out.println(myStrings.length);
+
+
+    }
+}
