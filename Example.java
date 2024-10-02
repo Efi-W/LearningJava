22f989b8055991beac91e1af8beb7c8ab7a0658d
EfiW <efi.whiteley@gmail.com>
2024-10-02 09:02:17 +0100

    multidimensional arrays

--- Example.java
+++ Example.java
@@ -7,9 +7,25 @@ public class Example
        int[] myInts = {3,4,5,6};
        myStrings[0] = "Hi";
 
+
         System.out.println(myStrings[0]);
         System.out.println(myStrings.length);
 
+        int[][] myNumbers = { {1, 2, 3, 4 }, {5, 6, 7, 8} };
+        System.out.println(myNumbers[1][3]);
 
+        myNumbers[1][2] = 9;
+        System.out.println(myNumbers[1][2]);
+
+
+        for (int i = 0; i < myStrings.length; i++) {
+            System.out.println(myStrings[i]);
+
+        }
+        for (int i = 0; i < myNumbers.length; ++i) {
+            for (int j = 0; j < myNumbers[i].length; ++j) {
+                System.out.println(myNumbers[i][j]);
+            }
+        }
     }
 }
