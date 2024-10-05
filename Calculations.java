fd6cbff7b694957c61556d512e61fb106fcb2eac
EfiW <efi.whiteley@gmail.com>
2024-10-05 13:38:45 +0100

    Inheritance

---
+++ Calculations.java
@@ -0,0 +1,13 @@
+public class Calculations
+{
+    public static void main(String[] args)
+    {
+        Inheritance obj = new Inheritance();
+        int r1 = obj.add(2,5);
+        int r2 = obj.sub(2,5);
+        int r3 = obj.multi(2,5);
+        int r4 = obj.div(5,10);
+
+        System.out.println(r1 + " " + r2+ " " + r3 + " " + r4);
+    }
+}
---
+++ Inheritance2.java
@@ -0,0 +1,12 @@
+public class Inheritance2
+{
+    public int multi(int n1, int n2)
+    {
+        return n1*n2;
+    }
+
+    public int div(int n1, int n2)
+    {
+        return n2/n1;
+    }
+}
